cecf6b6dc9126ae78ac1317a2975616c44f12b9a
Edward_MIT <usmanoff19980404@gmail.com>
2024-12-08 11:49:43 +0900

    feat: Integration of environmental variables

---
+++ src/train.ts
@@ -0,0 +1,19 @@
+// TASK G:
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+//  Misol yechilishi
+
+function getHighestIndex(arr: number[]): number {
+  let max = Math.max(...arr);
+  return arr.indexOf(max);
+}
+
+// Misol:
+console.log(getHighestIndex([5, 21, 12, 21, 8]));

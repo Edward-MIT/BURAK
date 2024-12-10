0c49691ba9123d2eaee76637fe1ad1c40bf1ec16
Edward_MIT <usmanoff19980404@gmail.com>
2024-12-10 22:28:08 +0900

    fix: modifing routing system and controllers

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,34 +2,11 @@
 import {Request, Response} from "express";
 import {T} from "../libs/types/common";
 
+// REACT
+
+
 
 const memberController: T = {};
 
-memberController.goHome = (req: Request, res: Response) =>{
-  try {
-    res.send("Home page");
-  } catch(err) {
-    console.log("Error on router/HomePage", err);
-  }
-
-};
-
-memberController.getLogin = (req: Request, res: Response) =>{
-  try {
-    res.send("Login page");
-  } catch(err) {
-    console.log("Error on router/LoginPage", err);
-  }
-
-};
-
-memberController.getSignup = (req: Request, res: Response) =>{
-  try {
-    res.send("SignUp page");
-  } catch(err) {
-    console.log("Error on router/SignUpPage", err);
-  }
-
-};
 
 export default memberController;
---
+++ src/controllers/restaurant.controller.ts
@@ -0,0 +1,34 @@
+import {Request, Response} from "express";
+import {T} from "../libs/types/common";
+
+
+const restaurantController: T = {};
+
+restaurantController.goHome = (req: Request, res: Response) =>{
+  try {
+    res.send("Home page");
+  } catch(err) {
+    console.log("Error on router/HomePage", err);
+  }
+
+};
+
+restaurantController.getLogin = (req: Request, res: Response) =>{
+  try {
+    res.send("Login page");
+  } catch(err) {
+    console.log("Error on router/LoginPage", err);
+  }
+
+};
+
+restaurantController.getSignup = (req: Request, res: Response) =>{
+  try {
+    res.send("SignUp page");
+  } catch(err) {
+    console.log("Error on router/SignUpPage", err);
+  }
+
+};
+
+export default restaurantController;

2d35884cc515c4306ac5cd8ce6646f9512810f53
Edward_MIT <usmanoff19980404@gmail.com>
2024-12-23 21:01:46 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,20 +5,40 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBstore = ConnectMongoDB(session);
+const store = new MongoDBstore({
+  uri: String (process.env.MONGO_URL),
+  collection : "sessions"
+});
+
+
+
 // expressning 4 ta bo'limi mavjud
 
 /* ENTERENCE */
 
-// console.log(__dirname);
-
 const app = express();
-// console.log("__dirname:", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /* SESSIONS */
+
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3 // 3 hours
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 
 /* VIEWS */

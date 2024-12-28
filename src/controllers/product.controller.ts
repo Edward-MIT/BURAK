15c704e2d0b2bc79bc233fef94a5053e0d50840b
Edward_MIT <usmanoff19980404@gmail.com>
2024-12-28 23:26:36 +0900

    feat: createNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,12 +1,20 @@
-import Errors from "../libs/Error";
+import Errors, { HttpCode, Message } from "../libs/Error";
 import {Request, Response} from "express";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 
 const productService = new ProductService();
 
 const productController: T ={};
+
+  /** SPA **/
+
+
+
+ /** SSR **/
 
 productController.getAllProducts = async (req:Request, res: Response) =>{
   try {
@@ -20,15 +28,28 @@ productController.getAllProducts = async (req:Request, res: Response) =>{
 
 };
 
-productController.createNewProduct = async (req: Request, res: Response) =>{
+productController.createNewProduct = async (req: AdminRequest, res: Response) =>{
   try {
     console.log("createNewProducts");
-    res.send("done");
+    console.log("req.files:", req.files);
+    if(!req.files?.length) throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map(ele => {
+    return ele.path.replace(/\\/g, "/");
+    });
+
+    // console.log("data", data);
+
+    await productService.createNewProduct(data);
+
+    res.send(`<script> alert("Successful creation!"); window.location.replace('admin/product/all') </script>`);
 
   } catch(err) {
     console.log("Error createNewProducts", err);
-    if(err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(`<script> alert("${message}"); window.location.replace('admin/product/all') </script>`);
+
   }
 
 };
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,4 +1,8 @@
+import Errors from "../libs/Error";
+import { Product, ProductInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
+import { HttpCode } from "../libs/Error";
+import { Message } from "../libs/Error";
 
 
 class ProductService {
@@ -7,8 +11,22 @@ class ProductService {
 
   constructor () {
     this.productModel = ProductModel;
+  }
+
+  /** SPA **/
+
+  /** SSR **/
+
+  public async createNewProduct(input: ProductInput): Promise<Product> {
+   try{
+     return await this.productModel.create(input);
+   }catch(err) {
+    console.log("Error, model:createNewProduct", err);
+    throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+   }
 
   }
+
 
 };
 

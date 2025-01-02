ea96d33eab9c65d8d6d812184076f04fd7863878
Edward_MIT <usmanoff19980404@gmail.com>
2025-01-02 20:08:23 +0900

    develop updateChosenUser API

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -41,7 +41,7 @@ routerAdmin.post("/product/:id",
 // User
 
 routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers );
-
+routerAdmin.post("/user/edit", restaurantController.verifyRestaurant, restaurantController.updateChosenUser );
 
 
 export default routerAdmin;

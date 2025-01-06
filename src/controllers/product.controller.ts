5734a78d43e3065089bcca37320700455dde87f3
Edward_MIT <usmanoff19980404@gmail.com>
2025-01-06 14:05:59 +0900

    feat: validateForm was created

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -46,12 +46,12 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
 
     await productService.createNewProduct(data);
 
-    res.send(`<script> alert("Successful creation!"); window.location.replace('admin/product/all') </script>`);
+    res.send(`<script> alert("Successful creation!"); window.location.replace('/admin/product/all') </script>`);
 
   } catch(err) {
     console.log("Error createNewProducts", err);
     const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-    res.send(`<script> alert("${message}"); window.location.replace('admin/product/all') </script>`);
+    res.send(`<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`);
 
   }
 

ee828011747e318f024ba4f10a3f0f075a9d2541
Edward_MIT <usmanoff19980404@gmail.com>
2025-01-26 18:33:22 +0900

    MIT task was done

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,17 @@
-// Task v
+//  Task W
+function chunkArray<T>(arr: T[], size: number): T[][] {
+  let result: T[][] = [];
+  for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
 
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+
+
+// Task v
+/*
 function countChars(str: string): Record<string, number> {
   let charCount: Record<string, number> = {};
 
@@ -11,7 +23,7 @@ function countChars(str: string): Record<string, number> {
 }
 
 console.log(countChars("hello"));
-
+*/
 
 // task U
 

26ed724f02771bc7d6c1df92eed6ce138d250b0e
Edward_MIT <usmanoff19980404@gmail.com>
2025-01-26 11:11:05 +0900

    feat:develop verify authenticated user credentials

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -5,7 +5,11 @@ import jwt from "jsonwebtoken";
 import { HttpCode } from "../libs/Error";
 
 class AuthService{
-  constructor() {}
+  private readonly secretToken;
+
+  constructor() {
+    this.secretToken = process.env.SECRET_TOKEN as string;
+  }
 
   public async createTooken(payload: Member) {
     return new Promise((resolve, reject) =>{
@@ -21,6 +25,14 @@ class AuthService{
 
 
   }
+
+ public async checkAuth(token: string): Promise<Member>{
+  const result:Member = (await jwt.verify(token, this.secretToken)) as Member;
+  console.log(`---[AUTH] memberNick: ${result.memberNick}----`);
+  return result;
+
+ }
+
 }
 
 export default AuthService;
